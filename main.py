6a0326f4ba5c65428fd14616c5babce0af3f843f
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:49:39 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,50 @@
+1. `list(map(lambda x: x**2, [1, 2, 3, 4, 5]))`
+2. `list(map(lambda x: x*2, [1, 2, 3, 4, 5]))`
+3. `list(map(lambda x: x+1, [1, 2, 3, 4, 5]))`
+4. `list(map(lambda x: x-1, [1, 2, 3, 4, 5]))`
+5. `list(map(lambda x: x**3, [1, 2, 3, 4, 5]))`
+6. `list(map(lambda x: x**4, [1, 2, 3, 4, 5]))`
+7. `list(map(lambda x: x**5, [1, 2, 3, 4, 5]))`
+8. `list(map(lambda x: x**6, [1, 2, 3, 4, 5]))`
+9. `list(map(lambda x: x**7, [1, 2, 3, 4, 5]))`
+10. `list(map(lambda x: x**8, [1, 2, 3, 4, 5]))`
+11. `list(map(lambda x: x**9, [1, 2, 3, 4, 5]))`
+12. `list(map(lambda x: x**10, [1, 2, 3, 4, 5]))`
+13. `list(map(lambda x: x+2, [1, 2, 3, 4, 5]))`
+14. `list(map(lambda x: x-2, [1, 2, 3, 4, 5]))`
+15. `list(map(lambda x: x*3, [1, 2, 3, 4, 5]))`
+16. `list(map(lambda x: x/2, [1, 2, 3, 4, 5]))`
+17. `list(map(lambda x: x/3, [1, 2, 3, 4, 5]))`
+18. `list(map(lambda x: x/4, [1, 2, 3, 4, 5]))`
+19. `list(map(lambda x: x/5, [1, 2, 3, 4, 5]))`
+20. `list(map(lambda x: x**2 + 2*x + 1, [1, 2, 3, 4, 5]))`
+21. `list(map(lambda x: x**2 - 2*x + 1, [1, 2, 3, 4, 5]))`
+22. `list(map(lambda x: x**2 + 3*x + 2, [1, 2, 3, 4, 5]))`
+23. `list(map(lambda x: x**2 - 3*x + 2, [1, 2, 3, 4, 5]))`
+24. `list(map(lambda x: x**2 + 4*x + 3, [1, 2, 3, 4, 5]))`
+25. `list(map(lambda x: x**2 - 4*x + 3, [1, 2, 3, 4, 5]))`
+26. `list(map(lambda x: x**2 + 5*x + 4, [1, 2, 3, 4, 5]))`
+27. `list(map(lambda x: x**2 - 5*x + 4, [1, 2, 3, 4, 5]))`
+28. `list(map(lambda x: x**2 + 6*x + 5, [1, 2, 3, 4, 5]))`
+29. `list(map(lambda x: x**2 - 6*x + 5, [1, 2, 3, 4, 5]))`
+30. `list(map(lambda x: x**2 + 7*x + 6, [1, 2, 3, 4, 5]))`
+31. `list(map(lambda x: x**2 - 7*x + 6, [1, 2, 3, 4, 5]))`
+32. `list(map(lambda x: x**2 + 8*x + 7, [1, 2, 3, 4, 5]))`
+33. `list(map(lambda x: x**2 - 8*x + 7, [1, 2, 3, 4, 5]))`
+34. `list(map(lambda x: x**2 + 9*x + 8, [1, 2, 3, 4, 5]))`
+35. `list(map(lambda x: x**2 - 9*x + 8, [1, 2, 3, 4, 5]))`
+36. `list(map(lambda x: x**2 + 10*x + 9, [1, 2, 3, 4, 5]))`
+37. `list(map(lambda x: x**2 - 10*x + 9, [1, 2, 3, 4, 5]))`
+38. `list(map(lambda x: x**2 + 11*x + 10, [1, 2, 3, 4, 5]))`
+39. `list(map(lambda x: x**2 - 11*x + 10, [1, 2, 3, 4, 5]))`
+40. `list(map(lambda x: x**2 + 12*x + 11, [1, 2, 3, 4, 5]))`
+41. `list(map(lambda x: x**2 - 12*x + 11, [1, 2, 3, 4, 5]))`
+42. `list(map(lambda x: x**2 + 13*x + 12, [1, 2, 3, 4, 5]))`
+43. `list(map(lambda x: x**2 - 13*x + 12, [1, 2, 3, 4, 5]))`
+44. `list(map(lambda x: x**2 + 14*x + 13, [1, 2, 3, 4, 5]))`
+45. `list(map(lambda x: x**2 - 14*x + 13, [1, 2, 3, 4, 5]))`
+46. `list(map(lambda x: x**2 + 15*x + 14, [1, 2, 3, 4, 5]))`
+47. `list(map(lambda x: x**2 - 15*x + 14, [1, 2, 3, 4, 5]))`
+48. `list(map(lambda x: x**2 + 16*x + 15, [1, 2, 3, 4, 5]))`
+49. `list(map(lambda x: x**2 - 16*x + 15, [1, 2, 3, 4, 5]))`
+50. `list(map(lambda x: x**2 + 17*x + 16, [1, 2, 3, 4, 5]))`
